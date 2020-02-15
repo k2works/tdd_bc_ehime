bd7bf475354adbeade70028cce4c28c0eb2c4b41
k2works <kakimomokuri@gmail.com>
2020-02-15 14:15:23 +0900

    test: TODO2

--- src/index.test.js
+++ src/index.test.js
@@ -4,17 +4,24 @@ const assert = chai.assert;
 suite("ClosedRangeTest", () => {
   suite("文字列表現を返す", () => {
     test("下端点 3, 上端点 8 ならば文字列[3,8]を返す", () => {
-      assert.equal("[3,8]", ClosedRange.stringify());
+      closedRange = new ClosedRange(3, 8);
+      assert.equal("[3,8]", closedRange.stringify());
     });
 
     test("下端点 4, 上端点 9 ならば文字列[4,9]を返す", () => {
-      assert.equal("[4,9]", ClosedRange.stringify());
+      closedRange = new ClosedRange(4, 9);
+      assert.equal("[4,9]", closedRange.stringify());
     });
   });
 });
 
 class ClosedRange {
-  static stringify() {
-    return "[3,8]";
+  constructor(lower, upper) {
+    this._lower = lower;
+    this._upper = upper;
+  }
+  stringify() {
+    let result = `[${this._lower},${this._upper}]`;
+    return result;
   }
 }
