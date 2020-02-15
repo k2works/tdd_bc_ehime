87adecc3853a18b6c30982891ca36b68a885d422
k2works <kakimomokuri@gmail.com>
2020-02-15 14:18:44 +0900

    refactor: 変数

--- src/index.test.js
+++ src/index.test.js
@@ -4,12 +4,12 @@ const assert = chai.assert;
 suite("ClosedRangeTest", () => {
   suite("文字列表現を返す", () => {
     test("下端点 3, 上端点 8 ならば文字列[3,8]を返す", () => {
-      closedRange = new ClosedRange(3, 8);
+      const closedRange = new ClosedRange(3, 8);
       assert.equal("[3,8]", closedRange.stringify());
     });
 
     test("下端点 4, 上端点 9 ならば文字列[4,9]を返す", () => {
-      closedRange = new ClosedRange(4, 9);
+      const closedRange = new ClosedRange(4, 9);
       assert.equal("[4,9]", closedRange.stringify());
     });
   });
